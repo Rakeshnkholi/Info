d0e3f6c4d3782039b2376fa8422e74adaa6f8342
mouna <mouna@gmail.com>
2025-02-25 23:17:18 +0530

    Added testclass for addcart

---
+++ src/test/java/com/sun/tests/Addcartdropdown.java
@@ -0,0 +1,13 @@
+package com.sun.tests;
+
+import org.testng.annotations.Test;
+
+public class Addcartdropdown 
+{
+	@Test
+	public void addcartdropdown()
+	{
+		System.out.println("Hello git i am updating files");
+	}
+
+}
